8a06f588bdee633f5bd83fad1215b9f241bb52ea
Soomin Hwang <soomin.h.hwang@gmail.com>
2019-07-27 23:15:50 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -13,7 +13,7 @@ def retrieval
                   "porcelain vases" => 2,
                   "oil paintings" => 3
                  }
-  shipping_manifest("oil paintings")
+  # Look up the value of the "oil paintings" key in the shipping_manifest hash below
 end
 
 def adding
@@ -22,14 +22,7 @@ def adding
                   "porcelain vases" => 2,
                   "oil paintings" => 3
                  }
-  # add 2 muskets to the shipping_manifest hash below
-
-
-
-  # add 4 gun powder to the shipping_manifest hash below
-
-
-
-  # return the shipping_manifest hash below
-
+  shipping_manifest["muskets"] = 2
+  shipping_manifest["gun powder"] = 4
+  shipping_manifesto
 end
