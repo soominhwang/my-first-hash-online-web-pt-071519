11350ba8d40e393ccda636b2f36c9b30d6df883b
Soomin Hwang <soomin.h.hwang@gmail.com>
2019-07-27 23:12:25 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,15 +1,14 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
+  your_hash = {"Name" => "Soomin", "Age" => 28, "Gender" => "Female"}
   
 end
-
 
 def shipping_manifest
   # set a variable called `the_manifest`, equal to a hash
   # fill that hash with key/value pairs that describe the following information: 
   # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
-
+  
 
 end
 
